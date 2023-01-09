579c71a3cf59c5e8716c14b4bceb3ab348136f88
Jessica Vaughn <jessica.vaughn619@gmail.com>
2023-01-09 16:58:43 -0700

    solution

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// code your solution here
+function superbowlWin(record, result) {
+   for (const team of record) {
+    if (team.result === "W") {
+        return team.year;
+    }
+   }
+};
+
+record.prototype.find(superbowlWin);
